6d79edeb8027f8fd92c8bfdff99c1cdaee03d750
kingvi <2198626335@qq.com>
2024-01-15 21:34:21 +0800

    最后一次提交

--- src/main/Java/pers/kingvi/foreigntrade/util/FileSet.java
+++ src/main/Java/pers/kingvi/foreigntrade/util/FileSet.java
@@ -4,7 +4,7 @@ package pers.kingvi.foreigntrade.util;
  * 对图片路径的引入，从此处设置
  */
 public class FileSet {
-/*//    外贸员头像保存路径
+//    外贸员头像保存路径
     public static final String FTS_IMAGE_PATH = "D:\\IDEAWorkSpace\\fts-image\\";
 //    货代头像保存路径
     public static final String FA_IMAGE_PATH = "D:\\IDEAWorkSpace\\fa-image\\";
@@ -15,10 +15,13 @@ public class FileSet {
 //    货代推荐图片保存位置
     public static final String RECOMMEND_PATH = "D:\\IDEAWorkSpace\\recommend-image\\";
 //    货代推荐审核图片保存位置
-    public static final String RECOMMEND_VERIFY_PATH = "D:\\IDEAWorkSpace\\recommend-verify-image\\";*/
+    public static final String RECOMMEND_VERIFY_PATH = "D:\\IDEAWorkSpace\\recommend-verify-image\\";
+//    设置最大上传图片为3, 为3145728字节
+    public static final int MAX_PHOTO_SIZE = 3145728;
 
 
-    //    外贸员头像保存路径
+
+   /* //    外贸员头像保存路径
     public static final String FTS_IMAGE_PATH = "C:\\imageSpace\\fts-image\\";
     //    货代头像保存路径
     public static final String FA_IMAGE_PATH = "C:\\imageSpace\\fa-image\\";
@@ -31,6 +34,6 @@ public class FileSet {
     //    货代推荐审核图片保存位置
     public static final String RECOMMEND_VERIFY_PATH = "C:\\imageSpace\\recommend-verify-image\\";
 //    设置最大上传图片为3, 为3145728字节
-    public static final int MAX_PHOTO_SIZE = 3145728;
+    public static final int MAX_PHOTO_SIZE = 3145728;*/
 }
 
